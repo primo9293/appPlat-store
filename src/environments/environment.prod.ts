358a4c79e002eee437bd2c9da18f4a4776641ea4
primo9293 <42128291+primo9293@users.noreply.github.com>
2020-08-24 21:43:17 -0500

    Firebase Deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  url_api : 'http://platzi-store.herokuapp.com/products/',
+  firebase : {
+    apiKey: 'AIzaSyA3AVq76A8SSaq8Y0uq5hVciYqflYjxa2A',
+    authDomain: 'platz-store.firebaseapp.com',
+    databaseURL: 'https://platz-store.firebaseio.com',
+    projectId: 'platz-store',
+    storageBucket: 'platz-store.appspot.com',
+    messagingSenderId: '1014650095340',
+    appId: '1:1014650095340:web:72937086ac1cf993e547c9'
+  }
 };
